428a24af14840d6456860d70ed631eacfe48795b
Gabriel P <gabopos@gmail.com>
2023-05-03 17:41:12 -0500

    Add React Intro Exercises

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import Botones from "./Botones"; 
 
-const studentName = "";
+const studentName = "Gabriel";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+
+  return(
+    <div>
+      <h1>Soy Gabriel!</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map((el, i)=> 
+          <li key={i}>{el}</li>
+        )}
+      </ul>
+
+      <Botones alerts={alerts}/>
+      
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
