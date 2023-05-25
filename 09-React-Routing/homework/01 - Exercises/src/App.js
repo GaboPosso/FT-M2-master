a4445a864799b2f84e96868758697c75f0b3f93b
Gabriel P <gabopos@gmail.com>
2023-05-24 23:57:37 -0500

    Update exercises 01 React Routing

--- 09-React-Routing/homework/01 - Exercises/src/App.js
+++ 09-React-Routing/homework/01 - Exercises/src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { Route, Routes} from 'react-router-dom';
 /* eslint-disable */
 import Home from "./components/Home/Home.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Shipping from "./components/Shipping/Shipping.jsx";
 import Discounts from "./components/Discounts/Discounts.jsx";
 import CardDetail from "./components/CardDetail/CardDetail.jsx";
+import Promotions from "./components/Discounts/Discounts.jsx";
 /* eslint-disable */
 
 export default function App() {
-  return <div></div>;
+  return (
+  <div>
+    <NavBar/> 
+    <Routes>
+      <Route path="/" Component={<Home/>} />
+      <Route path="/shipping" Component={<Shipping/>} />
+      <Route path="/discounts" Component={<Discounts/>} />
+      <Route path="/promotions" Component={<Promotions/>} /> 
+      <Route path="/cruises/:id" Component={<CardDetail/>} />
+
+    </Routes>
+  </div>);
 }
